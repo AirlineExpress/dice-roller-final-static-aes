2df583c15b7ea00aa274dd2d9133ac7971b04eb5
AirlineExpress <samueljswedo@lewisu.edu>
2024-10-04 22:53:31 -0500

    Added it so it gets dice rolls from node server

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dice-roller-final-static-aes
+# Samuel Swedo
+# samueljswedo@lewisu.edu
+# Software Engineering
+
+# Web Dice Roller
+# Credit: Eric Pogue for potpourri code for some methods, and for the MernaNodejs Template
+# Needed files include: index.html, web-dice-roller.css, web-dice-roller-script.js
+
+# To view file, open index.html in a web browser
---
+++ web-dice-roller-script.js
@@ -0,0 +1,38 @@
+function diceRoller() {
+
+    /* This will assign the dice to their slots and then roll their value*/
+    const dice1 = document.getElementById("dice1");
+    const dice2 = document.getElementById("dice2");
+    const dice3 = document.getElementById("dice3");
+    const dice4 = document.getElementById("dice4");
+    const dice5 = document.getElementById("dice5");
+
+    dice1.value = Math.floor(Math.random()*6)+1;
+    dice2.value = Math.floor(Math.random()*6)+1;
+    dice3.value = Math.floor(Math.random()*6)+1;
+    dice4.value = Math.floor(Math.random()*6)+1;
+    dice5.value = Math.floor(Math.random()*6)+1;
+}
+
+async function diceRoller() {
+
+    /* This will assign the dice to their slots and then roll their value*/
+    /* This will also try to fetch the value from the node server and show error if it can not*/
+    try {
+        const website = await fetch('https://dice-roller-nodejs-aes-cmcjh6fccbchefae.centralus-01.azurewebsites.net/rolldice');
+        const data = await website.json();
+        const dice1 = document.getElementById("dice1");
+        const dice2 = document.getElementById("dice2");
+        const dice3 = document.getElementById("dice3");
+        const dice4 = document.getElementById("dice4");
+        const dice5 = document.getElementById("dice5");
+        dice1.value = data.dice[4];
+        dice2.value = data.dice[3];
+        dice3.value = data.dice[2];
+        dice4.value = data.dice[1];
+        dice5.value = data.dice[0];
+    }
+    catch (error) {
+        console.error('Error', error);
+    }
+}
